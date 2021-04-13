5877ec350fed933660509a03972a56b0138731b7
Tomas Oh <>
2021-04-13 20:50:18 +0000

    Added AP Practice FRQ

--- Delimiters.java
+++ Delimiters.java
@@ -10,14 +10,36 @@ public class Delimiters
 		openDel = open;
 		closeDel = close;
 	}
+  //Methods getDelimetersList and isBalanced by: Tomas Oh, Period 2
 
 	public ArrayList<String> getDelimitersList(String[] tokens)
 	{
-		return null;
-	}
+    ArrayList<String> list = new ArrayList<String>();
+    for(int x = 0; x < tokens.length; x++)
+    {
+      if(tokens[x].equals(openDel) || tokens[x].equals(closeDel))
+      {
+        list.add(tokens[x]);
+      }
+    }
+   return list;
+  }
 
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
-		return false;
+		int num = 0;
+    for(int x = 0; x < delimiters.size(); x++)
+    {
+      if(delimiters.get(x).equals(openDel)){
+        num++;
+      }
+      if(delimiters.get(x).equals(closeDel)){
+        num--;
+      }
+      if(num < 0){
+        return false;
+      }
+    }
+   return num == 0;
 	}
 }
